bc20142579f86df3c7771c40066c0291ec603d58
AlfrSach <115001779+AlfrSach@users.noreply.github.com>
2023-02-27 17:45:11 +0530

    Add files via upload

--- My_Proj/pages/Sales.py
+++ My_Proj/pages/Sales.py
@@ -2,14 +2,27 @@ import streamlit as st
 import pandas as pd
 from matplotlib import image
 import plotly.express as px
+import os
+
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "image", "sales.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "all_data.csv")
+
 
 st.title("Dashboard - Sales Data")
 
-img = image.imread("resources/image/sales.jpg")
+img = image.imread(IMAGE_PATH)
 st.image(img)
 
 
-dataset = pd.read_csv(r"C:\Users\tripl\OneDrive\Desktop\Python\Innomatics Internship\My_Proj\resources\data\all_data.csv")
+dataset = pd.read_csv(DATA_PATH)
 st.dataframe(dataset)
 
 dataset.dropna(inplace=True)
@@ -25,7 +38,7 @@ col1, col2 = st.columns(2)
 
 data_ms = updated_dataset.groupby("Month").sum()
 fig1 = px.bar(data_ms, x=data_ms.index, y="Sales", labels={"x": "Month", "y": "Sales"}, title="Monthly Sales")
-fig1.show()
+
 col1.plotly_chart(fig1, use_container_width=True)
 
 txt = "December has the highest sales and January has the lowest sales"
